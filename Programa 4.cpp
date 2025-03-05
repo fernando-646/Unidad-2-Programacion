8555f11ad0f824228011773d272af1caff1a1007
Fernando Ramos Cota <79738655+fernando-646@users.noreply.github.com>
2025-03-05 11:45:15 -0600

    Update Programa 4.cpp

--- Programa 4.cpp
+++ Programa 4.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     float SUELDO, NUEVOSUELDO, AUMENTO;
-    cout<<"Programa 2, Unidad 2 :)" << endl;
+    cout<<"Programa 4, Unidad 2 :)" << endl;
     cout<<"Porfavor ingrese el salario del trabajador  " << endl;
     cin>>SUELDO;
     if (SUELDO <= 400000.0) {
